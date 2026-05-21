7ce2e88bfcabc40de1c3a1c1a27dece80f71df92
NahuelRn <129953596+NahuelRn@users.noreply.github.com>
2026-05-20 22:21:31 -0300

    Fix del Merge de Combate y de Test Varios

--- src/main/java/com/tallerwebi/dominio/Partida.java
+++ src/main/java/com/tallerwebi/dominio/Partida.java
@@ -4,21 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.*;
 
+@Entity
 public class Partida {
 
-//  private Integer identificadorPartida;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @ManyToOne
+    private Enemigo enemigo;
+
+    // --- VARIABLES COMPARTIDAS ---
     private Integer hpJugador;
     private Integer hpEnemigo;
 
+    // --- LO DE MIKA (Combate) ---
     private Integer turno;
-
-//  private Usuario usuario;
-//  private Enemigo enemigo;
-
     private String estado;
 
-    private List<Integer> cartasEnManoJugador;
+    @Transient
+    private List<Carta> manoJugador = new ArrayList<>();
 
+    @Transient
+    private List<Integer> cartasEnManoJugador = new ArrayList<>();
+
+    // --- CONSTRUCTORES ---
+    public Partida() {
+    }
+
+    // 2. Constructor que armó Mika para su lógica
     public Partida(Integer hpJugador, Integer hpEnemigo, Integer turno) {
         this.hpJugador = hpJugador;
         this.hpEnemigo = hpEnemigo;
@@ -26,39 +43,83 @@ public class Partida {
         this.cartasEnManoJugador = new ArrayList<>();
     }
 
-    public Integer getHpJugador() {
-        return this.hpJugador;
+    // GETTERS Y SETTERS
+
+    public Long getId() {
+        return id;
     }
 
-    public void setHpJugador(Integer hpJugador) {
+    public Partida setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Integer getHpJugador() {
+        return hpJugador;
+    }
+
+    public Partida setHpJugador(Integer hpJugador) {
         this.hpJugador = hpJugador;
+        return this;
     }
 
     public Integer getHpEnemigo() {
-        return this.hpEnemigo;
+        return hpEnemigo;
     }
 
-    public void setHpEnemigo(Integer hpEnemigo) {
+    public Partida setHpEnemigo(Integer hpEnemigo) {
         this.hpEnemigo = hpEnemigo;
+        return this;
     }
 
     public Integer getTurno() {
-        return this.turno;
+        return turno;
     }
 
     public void setTurno(Integer turno) {
         this.turno = turno;
     }
 
-    public void setCartasEnManoJugador(List<Integer> cartasEnManoJugador) {
-        this.cartasEnManoJugador = cartasEnManoJugador;
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Partida setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+        return this;
+    }
+
+    public Enemigo getEnemigo() {
+        return enemigo;
+    }
+
+    public Partida setEnemigo(Enemigo enemigo) {
+        this.enemigo = enemigo;
+        return this;
+    }
+
+    public List<Carta> getManoJugador() {
+        return manoJugador;
+    }
+
+    public Partida setManoJugador(List<Carta> manoJugador) {
+        this.manoJugador = manoJugador;
+        return this;
     }
 
     public List<Integer> getCartasEnManoJugador() {
         return cartasEnManoJugador;
     }
 
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public void setCartasEnManoJugador(List<Integer> cartasEnManoJugador) {
+        this.cartasEnManoJugador = cartasEnManoJugador;
     }
 }
