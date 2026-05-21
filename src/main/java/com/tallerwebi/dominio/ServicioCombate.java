4dfa6aa71d51ad3dfca731083166524a207769ed
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-05-21 06:40:35 -0300

    Se modificó ServicioCombate.

--- src/main/java/com/tallerwebi/dominio/ServicioCombate.java
+++ src/main/java/com/tallerwebi/dominio/ServicioCombate.java
@@ -1,96 +1,103 @@
 package com.tallerwebi.dominio;
 
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ServicioCombate {
 
-    public Integer jugarCarta(Integer identificadorCarta, Integer identificadorPartida) {
-        validarTurno(identificadorPartida);
-        validarCartaEnMano(identificadorCarta, identificadorPartida);
+  private static final int TURNO_JUGADOR = 1;
+  private static final int TURNO_ENEMIGO = 2;
 
-        Integer danioCartaHaciaEnemigo = calcularEfectoCarta(identificadorCarta);
+  private static final int PARTIDA_DE_PRUEBA = 1;
 
-        Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
-        partida.setHpEnemigo(partida.getHpEnemigo() - danioCartaHaciaEnemigo);
-        actualizarEstadoPartida(partida);
+  private static final int MAX_CARTAS_REPETIDAS = 3;
 
-        return danioCartaHaciaEnemigo;
+  private Partida partida = new Partida(100, 100, 1);
+
+  private int contadorCartasRepetidas = 0;
+
+  public Integer jugarCarta(Integer identificadorCarta, Integer identificadorPartida) {
+    validarTurno(identificadorPartida);
+    validarCartaEnMano(identificadorCarta, identificadorPartida);
+
+    Integer danioCartaHaciaEnemigo = calcularEfectoCarta();
+
+    Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
+    partida.setHpEnemigo(partida.getHpEnemigo() - danioCartaHaciaEnemigo);
+    actualizarEstadoPartida(partida);
+
+    return danioCartaHaciaEnemigo;
+  }
+
+  private void validarTurno(Integer identificadorPartida) {
+    Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
+
+    if (partida.getTurno() == null) {
+      throw new RuntimeException("Error, turno no definido.");
     }
 
-    private void validarTurno(Integer identificadorPartida) {
-        Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
+    if (partida.getTurno() != TURNO_JUGADOR) {
+      throw new RuntimeException("Turno del enemigo.");
+    }
+  }
 
-        if (partida.getTurno() == null) {
-            throw new RuntimeException("Error, turno no definido.");
-        }
+  private void validarCartaEnMano(Integer identificadorCarta, Integer identificadorPartida) {
+    Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
 
-        if (partida.getTurno() != 1) {
-            throw new RuntimeException("Turno del enemigo.");
-        }
+    if (partida.getCartasEnManoJugador() == null || partida.getCartasEnManoJugador().size() == 0) {
+      throw new RuntimeException("Error, el jugador no tiene cartas en mano.");
     }
 
-    private void validarCartaEnMano(Integer identificadorCarta, Integer identificadorPartida) {
-        Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
+    for (Integer carta : partida.getCartasEnManoJugador()) {
+      if (carta.equals(identificadorCarta)) {
+        contadorCartasRepetidas++;
 
-        if (partida.getCartasEnManoJugador() == null || partida.getCartasEnManoJugador().size() == 0) {
-            throw new RuntimeException("Error, el jugador no tiene cartas en mano.");
+        if (contadorCartasRepetidas > MAX_CARTAS_REPETIDAS) {
+          throw new RuntimeException("Error, máximo 3 cartas del mismo tipo.");
         }
+      }
+    }
+  }
 
-        Integer contadorCartasRepetidas = 0;
-        for (Integer buscarCartaIdentificador : partida.getCartasEnManoJugador()) {
-            if (buscarCartaIdentificador.equals(identificadorCarta) == true) {
-                contadorCartasRepetidas++;
-            }
-        }
+  private Integer calcularEfectoCarta() {
+    //      Formula -> danio = valor_base_carta * multiplicador + factor_suerte
 
-        if (contadorCartasRepetidas > 3) {
-            throw new RuntimeException("Error, el máximo de cartas repetidas es de 3.");
-        }
+    //      Ejemplo:
+    int valorBaseCarta = 10;
+    int multiplicador = 5;
+    int factorSuerte = 5;
+
+    return valorBaseCarta * multiplicador + factorSuerte;
+  }
+
+  public Partida obtenerPartidaPorIdentificador(Integer identificadorPartida) {
+    //      Aca va la logica del repositorio...
+
+    if (identificadorPartida == PARTIDA_DE_PRUEBA) {
+      ArrayList<Integer> cartas = new ArrayList<>();
+
+      cartas.add(1);
+      cartas.add(2);
+      cartas.add(3);
+
+      this.partida.setCartasEnManoJugador(cartas);
+
+      return this.partida;
     }
 
-    private Integer calcularEfectoCarta(Integer identificadorCarta) {
-//      Formula -> danio = valor_base_carta * multiplicador + factor_suerte;}
+    return new Partida(100, 100, TURNO_JUGADOR);
+  }
 
-//      Ejemplo:
-        Integer valorBaseCarta = 10;
-        Integer multiplicador = 5;
-        Integer factorSuerte = 5;
-
-        return valorBaseCarta * multiplicador + factorSuerte;
+  private void actualizarEstadoPartida(Partida partida) {
+    if (partida.getTurno() == TURNO_JUGADOR) {
+      partida.setTurno(TURNO_ENEMIGO);
     }
 
-    public Partida obtenerPartidaPorIdentificador(Integer identificadorPartida) {
-//      Aca va la logica del repositorio...
-
-        if (identificadorPartida == 1) {
-
-            Partida partida = new Partida(100, 100, 1);
-            ArrayList<Integer> cartas = new ArrayList<>();
-
-            cartas.add(1);
-            cartas.add(2);
-            cartas.add(3);
-
-            partida.setCartasEnManoJugador(cartas);
-
-            return partida;
-        }
-
-        return new Partida(100, 100, 2);
+    if (partida.getHpEnemigo() <= 0) {
+      partida.setEstado("Ganador jugador.");
+    } else if (partida.getHpJugador() <= 0) {
+      partida.setEstado("Ganador enemigo.");
     }
-
-    private void actualizarEstadoPartida(Partida partida) {
-        if (partida.getTurno() == 1) {
-            partida.setTurno(2);
-        }
-
-        if (partida.getHpEnemigo() <= 0) {
-            partida.setEstado("Ganador jugador.");
-        } else if (partida.getHpJugador() <= 0) {
-            partida.setEstado("Ganador enemigo.");
-        }
-    }
+  }
 }
