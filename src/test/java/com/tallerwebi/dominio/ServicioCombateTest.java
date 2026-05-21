1c9159b48402ea92595cdc49f38ea441c39470ea
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-05-21 06:45:08 -0300

    Se agregó ServicioCombateTest.

---
+++ src/test/java/com/tallerwebi/dominio/ServicioCombateTest.java
@@ -0,0 +1,55 @@
+package com.tallerwebi.dominio;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+public class ServicioCombateTest {
+
+  ServicioCombate servicioCombate = new ServicioCombate();
+
+  @Test
+  public void deberiaJugarCartaCorrectamente() {
+    Integer danio = this.servicioCombate.jugarCarta(1, 1);
+
+    assertEquals(55, danio);
+  }
+
+  @Test
+  public void deberiaLanzarErrorCuandoNoEsTurnoDelJugador() {
+    RuntimeException error = assertThrows(
+      RuntimeException.class,
+      () -> servicioCombate.jugarCarta(1, 2)
+    );
+    assertEquals("Turno del enemigo.", error.getMessage());
+  }
+
+  @Test
+  public void deberiaCambiarTurnoLuegoDeJugarCarta() {
+    servicioCombate.jugarCarta(1, 1);
+
+    Partida partida = this.servicioCombate.obtenerPartidaPorIdentificador(1);
+
+    assertEquals(2, partida.getTurno());
+  }
+
+  @Test
+  public void deberiaLanzarErrorCuandoNoTieneCartasEnMano() {
+    Partida partida = this.servicioCombate.obtenerPartidaPorIdentificador(1);
+    partida.setCartasEnManoJugador(new ArrayList<>());
+
+    assertThrows(RuntimeException.class, () -> servicioCombate.jugarCarta(1, 2));
+  }
+
+  @Test
+  public void deberiaColocarEstadoGanadorJugadorCuandoHpEnemigoEsMenorOIgualACero() {
+    Partida partida = this.servicioCombate.obtenerPartidaPorIdentificador(1);
+    partida.setHpEnemigo(50);
+
+    servicioCombate.jugarCarta(1, 1);
+
+    assertEquals("Ganador jugador.", partida.getEstado());
+  }
+}
