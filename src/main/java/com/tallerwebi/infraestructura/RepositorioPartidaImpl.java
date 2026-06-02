51d9e5dd243f349e5ee883296e0bccaafc24d096
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-06-02 20:03:14 -0300

    Se agregó la implementación de RepositorioPartidaImpl, permitiendo la búsqueda de Partida por identificador.

---
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioPartidaImpl.java
@@ -0,0 +1,22 @@
+package com.tallerwebi.infraestructura;
+
+import com.tallerwebi.dominio.Partida;
+import com.tallerwebi.dominio.RepositorioPartida;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RepositorioPartidaImpl implements RepositorioPartida {
+
+    private SessionFactory sessionFactory;
+
+    public RepositorioPartidaImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Partida buscarPartidaPorIdentificador(Long identificador) {
+        return (Partida) this.sessionFactory.getCurrentSession().createCriteria(Partida.class).add(Restrictions.eq("id", identificador)).uniqueResult();
+    }
+}
