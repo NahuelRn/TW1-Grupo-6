553a968f644f4e1f8f13f7443cdcee096fc6bb90
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-06-02 19:58:46 -0300

    Se modificó la clase ServicioCombate a ServicioCombateImpl y se separó la lógica de actualización de estado y turno.

---
+++ src/main/java/com/tallerwebi/dominio/ServicioCombateImpl.java
@@ -0,0 +1,117 @@
+package com.tallerwebi.dominio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ServicioCombateImpl implements ServicioCombate {
+
+  private static final int TURNO_JUGADOR = 1;
+  private static final int TURNO_ENEMIGO = 2;
+
+  private static final int PARTIDA_DE_PRUEBA = 1;
+
+  private static final int MAX_CARTAS_REPETIDAS = 3;
+
+  private Partida partida = new Partida(100, 100, 1);
+
+  private int contadorCartasRepetidas = 0;
+
+  //private RepositorioPartida repositorioPartida;
+//
+//@Autowired
+//public ServicioCombateImpl(RepositorioPartida repositorioPartida) {
+//  this.repositorioPartida = repositorioPartida;
+//}
+
+  private RepositorioPartida repositorioPartida;
+
+  @Autowired
+  public ServicioCombateImpl(RepositorioPartida repositorioPartida) {
+    this.repositorioPartida = repositorioPartida;
+  }
+
+  @Override
+  public Integer jugarCarta(Integer identificadorCarta, Long identificadorPartida) {
+    validarTurno(identificadorPartida);
+    validarCartaEnMano(identificadorCarta, identificadorPartida);
+
+    Integer danioCartaHaciaEnemigo = calcularEfectoCarta();
+
+    Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
+    partida.setHpEnemigo(partida.getHpEnemigo() - danioCartaHaciaEnemigo);
+    actualizarEstadoPartida(partida);
+
+    return danioCartaHaciaEnemigo;
+  }
+
+  private void validarTurno(Long identificadorPartida) {
+    Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
+
+    if (partida.getTurno() == null) {
+      throw new RuntimeException("Error, turno no definido.");
+    }
+
+    if (partida.getTurno() != TURNO_JUGADOR) {
+      throw new RuntimeException("Turno del enemigo.");
+    }
+  }
+
+  private void validarCartaEnMano(Integer identificadorCarta, Long identificadorPartida) {
+    Partida partida = obtenerPartidaPorIdentificador(identificadorPartida);
+
+    if (partida.getCartasEnManoJugador() == null || partida.getCartasEnManoJugador().size() == 0) {
+      throw new RuntimeException("Error, el jugador no tiene cartas en mano.");
+    }
+
+    for (Integer carta : partida.getCartasEnManoJugador()) {
+      if (carta.equals(identificadorCarta)) {
+        contadorCartasRepetidas++;
+
+        if (contadorCartasRepetidas > MAX_CARTAS_REPETIDAS) {
+          throw new RuntimeException("Error, máximo 3 cartas del mismo tipo.");
+        }
+      }
+    }
+  }
+
+  private Integer calcularEfectoCarta() {
+    //      Formula -> danio = valor_base_carta * multiplicador + factor_suerte
+
+    //      Ejemplo:
+    int valorBaseCarta = 10;
+    int multiplicador = 5;
+    int factorSuerte = 5;
+
+    return valorBaseCarta * multiplicador + factorSuerte;
+  }
+
+  @Override
+  public Partida obtenerPartidaPorIdentificador(Long identificadorPartida) {
+    return this.repositorioPartida.buscarPartidaPorIdentificador(identificadorPartida);
+  }
+
+  private void actualizarEstadoPartida(Partida partida) {
+    cambiarTurno(partida);
+    cambiarEstado(partida);
+  }
+
+  private void cambiarTurno(Partida partida) {
+    if (partida.getTurno() == TURNO_JUGADOR) {
+      partida.setTurno(TURNO_ENEMIGO);
+    } else {
+      partida.setTurno(TURNO_JUGADOR);
+    }
+  }
+
+  private void cambiarEstado(Partida partida) {
+    if (partida.getHpEnemigo() <= 0) {
+      partida.setEnumEstadoPartida(EnumEstadoPartida.GANADOR_JUGADOR);
+    } else if (partida.getHpJugador() <= 0) {
+      partida.setEnumEstadoPartida(EnumEstadoPartida.GANADOR_ENEMIGO);
+    }
+  }
+}
