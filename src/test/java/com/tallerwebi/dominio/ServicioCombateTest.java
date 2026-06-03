debaaf28603d3bed596977beee50d475d7de237f
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-06-03 07:21:10 -0300

    Se modificaron algunos tests de la clase ServicioCombateTest.

--- src/test/java/com/tallerwebi/dominio/ServicioCombateTest.java
+++ src/test/java/com/tallerwebi/dominio/ServicioCombateTest.java
@@ -2,45 +2,66 @@ package com.tallerwebi.dominio;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 public class ServicioCombateTest {
 
-  ServicioCombate servicioCombate = new ServicioCombate();
+  RepositorioPartida repositorioPartida = mock(RepositorioPartida.class);
+  ServicioCombate servicioCombate = new ServicioCombateImpl(repositorioPartida);
 
   @Test
-  public void deberiaJugarCartaCorrectamente() {
-    Integer danio = this.servicioCombate.jugarCarta(1, 1);
+  public void deberiaCalcularCorrectamenteElDanioDeLaCarta() {
+    Partida partida = new Partida(100, 100, 1);
+    when(this.repositorioPartida.buscarPartidaPorIdentificador(1L)).thenReturn(partida);
 
-    assertEquals(55, danio);
+    ArrayList<Integer> cartasEnMano = new ArrayList<>();
+    cartasEnMano.add(1);
+    partida.setCartasEnManoJugador(cartasEnMano);
+
+    Integer danioEfecto = this.servicioCombate.jugarCarta(1, 1L);
+    assertEquals(55, danioEfecto);
   }
 
   @Test
   public void deberiaCambiarTurnoLuegoDeJugarCarta() {
-    servicioCombate.jugarCarta(1, 1);
+    Partida partida = new Partida(100, 100, 1);
+    when(this.repositorioPartida.buscarPartidaPorIdentificador(1L)).thenReturn(partida);
 
-    Partida partida = this.servicioCombate.obtenerPartidaPorIdentificador(1);
+    ArrayList<Integer> cartasEnMano = new ArrayList<>();
+    cartasEnMano.add(1);
+    partida.setCartasEnManoJugador(cartasEnMano);
 
+    this.servicioCombate.jugarCarta(1, 1L);
     assertEquals(2, partida.getTurno());
   }
 
   @Test
   public void deberiaLanzarErrorCuandoNoTieneCartasEnMano() {
-    Partida partida = this.servicioCombate.obtenerPartidaPorIdentificador(1);
+    Partida partida = new Partida(100, 100, 1);
+    when(this.repositorioPartida.buscarPartidaPorIdentificador(1L)).thenReturn(partida);
+
     partida.setCartasEnManoJugador(new ArrayList<>());
 
-    assertThrows(RuntimeException.class, () -> servicioCombate.jugarCarta(1, 2));
+    assertThrows(RuntimeException.class, () -> this.servicioCombate.jugarCarta(1, 1L));
   }
 
   @Test
   public void deberiaColocarEstadoGanadorJugadorCuandoHpEnemigoEsMenorOIgualACero() {
-    Partida partida = this.servicioCombate.obtenerPartidaPorIdentificador(1);
+    Partida partida = new Partida(100, 100, 1);
+    when(this.servicioCombate.obtenerPartidaPorIdentificador(1L)).thenReturn(partida);
+
+    ArrayList<Integer> cartasEnMano = new ArrayList<>();
+    cartasEnMano.add(1);
+    partida.setCartasEnManoJugador(cartasEnMano);
+
     partida.setHpEnemigo(50);
 
-    servicioCombate.jugarCarta(1, 1);
-
-    assertEquals("Ganador jugador.", partida.getEstado());
+    this.servicioCombate.jugarCarta(1, 1L);
+    assertEquals(EnumEstadoPartida.GANADOR_JUGADOR, partida.getEnumEstadoPartida());
   }
 }
