4f515bd90fabddab9f0fc7e31dc43a582d976cd5
Mika <227877359+Mikaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-07-01 11:12:56 -0300

    Se corrigieron 2 tests de la capa controlador para que den verde.

--- src/test/java/com/tallerwebi/presentacion/ControladorHistorialTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorHistorialTest.java
@@ -1,35 +1,40 @@
-//package com.tallerwebi.presentacion;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//import com.tallerwebi.dominio.*;
-//import java.util.ArrayList;
-//import org.junit.Test;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//public class ControladorHistorialTest {
-//
-//  private RepositorioHistorialPartida repositorioHistorialPartida = mock(
-//    RepositorioHistorialPartida.class
-//  );
-//  private ServicioHistorial servicioHistorial = new ServicioHistorialImpl(
-//    repositorioHistorialPartida
-//  );
-//
-//  @Test
-//  public void deberiaMostrarVistaHistorial() {
-//    ControladorHistorialPartida controladorHistorialPartida = new ControladorHistorialPartida(
-//      this.servicioHistorial
-//    );
-//
-//    ArrayList<HistorialPartida> lista = new ArrayList<>();
-//
-//    when(this.repositorioHistorialPartida.listarPorUsuario(1L)).thenReturn(lista);
-//
-//    ModelAndView modelAndView = controladorHistorialPartida.mostrarHistorialPartida(1L);
-//
-//    assertEquals("historial", modelAndView.getViewName());
-//  }
-//}
+package com.tallerwebi.presentacion;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.tallerwebi.dominio.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class ControladorHistorialTest {
+
+    private RepositorioHistorialPartida repositorioHistorialPartida = mock(RepositorioHistorialPartida.class);
+    private ServicioHistorial servicioHistorial = new ServicioHistorialImpl(repositorioHistorialPartida);
+
+    @Test
+    public void deberiaMostrarVistaHistorial() {
+        ControladorHistorialPartida controladorHistorialPartida = new ControladorHistorialPartida(this.servicioHistorial);
+
+        HttpServletRequest requestMock = mock(HttpServletRequest.class);
+        HttpSession sessionMock = mock(HttpSession.class);
+
+        when(requestMock.getSession()).thenReturn(sessionMock);
+        when(sessionMock.getAttribute("USUARIO_ID")).thenReturn(1L);
+
+        List<HistorialPartida> lista = new ArrayList<>();
+        when(this.servicioHistorial.listarHistorialPorUsuario(1L)).thenReturn(lista);
+
+        ModelAndView modelAndView = controladorHistorialPartida.mostrarHistorialPartida(requestMock);
+
+        assertEquals("historial", modelAndView.getViewName());
+    }
+}
